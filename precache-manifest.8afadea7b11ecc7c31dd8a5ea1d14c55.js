38a89093834a3d41dfa56a54ce7a95f389e1f1a2
shreyabadarkhe01 <shreyabadarkheofficial@gmail.com>
2022-09-08 12:25:59 +0530

    Updates

---
+++ precache-manifest.8afadea7b11ecc7c31dd8a5ea1d14c55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b554a4fdea6428dc159a",
+    "url": "/TextUtils/static/css/main.f0bbb9a7.chunk.css"
+  },
+  {
+    "revision": "b554a4fdea6428dc159a",
+    "url": "/TextUtils/static/js/main.8acbf2ff.chunk.js"
+  },
+  {
+    "revision": "7ac67eb9dc4c279ca082",
+    "url": "/TextUtils/static/js/runtime~main.b4ab1d75.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/TextUtils/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/TextUtils/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "723446d23f8e2641fcadf090faa18576",
+    "url": "/TextUtils/index.html"
+  }
+];
